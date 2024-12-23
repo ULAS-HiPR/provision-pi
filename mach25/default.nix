abd820740233476bc40a33393fd1a84c8d9b087c
Adam Byrne <adam.r.byrne@gmail.com>
2024-12-23 22:16:14 +0000

    enable wireless networking and configure SSH settings in default.nix

--- mach25/default.nix
+++ mach25/default.nix
@@ -9,8 +9,20 @@
       wlan0.useDHCP = true;
       eth0.useDHCP = true;
     };
+    wireless = {
+      enable = true;
+      interfaces = [ "wlan0" ];
+      networks = {
+        "coldspot" = {
+          psk = "helloworld1";
+        };
+      };
+    };
   };
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+  };
   raspberry-pi-nix.board = "bcm2711";
   hardware = {
     raspberry-pi = {
